57720d5bb1d9ea006c7c36825fb925ddd79a0ca3
Julian Perrott <julian_perrott@hotmail.com>
2018-05-11 15:20:57 +0100

    Change font to Impact

--- src/components/word-cloud/word-cloud.ts
+++ src/components/word-cloud/word-cloud.ts
@@ -29,10 +29,10 @@ export class WordCloudComponent implements OnChanges {
         if (this.lastdata == this.wordData.data) {
             return;
         }
-        
+
         this.setup();
         this.buildSVG();
-    
+
         this.lastdata = this.wordData.data;
 
         const ignoreWords = "a's,able,about,above,according,accordingly,across,actually,after,afterwards,again,against,ain't,all,allow,allows,almost,alone,along,already,also,although,always,am,among,amongst,an,and,another,any,anybody,anyhow,anyone,anything,anyway,anyways,anywhere,apart,appear,appreciate,appropriate,are,aren't,around,as,aside,ask,asking,associated,at,available,away,awfully,be,became,because,become,becomes,becoming,been,before,beforehand,behind,being,believe,below,beside,besides,best,better,between,beyond,both,brief,but,by,c,c'mon,c's,came,campaign,can,can't,cannot,cant,cause,causes,certain,certainly,changes,clearly,co,com,come,comes,concerning,consequently,consider,considering,contain,containing,contains,corresponding,could,couldn't,course,currently,definitely,described,despite,did,didn't,different,do,does,doesn't,doing,don't,done,down,downwards,during,each,edu,eight,either,else,elsewhere,enough,endorsed,entirely,especially,et,etc,even,ever,every,everybody,everyone,everything,everywhere,ex,exactly,example,except,far,few,fifth,first,financial,five,followed,following,follows,for,former,formerly,forth,four,from,further,furthermore,get,gets,getting,given,gives,go,goes,going,gone,got,gotten,greetings,had,hadn't,happens,hardly,has,hasn't,have,haven't,having,he,he's,hello,help,hence,her,here,here's,hereafter,hereby,herein,hereupon,hers,herself,hi,him,himself,his,hither,hopefully,how,howbeit,however,i'd,i'll,i'm,i've,if,ignored,immediate,in,inasmuch,inc,indeed,indicate,indicated,indicates,inner,insofar,instead,into,inward,is,isn't,it,it'd,it'll,it's,its,itself,just,keep,keeps,kept,know,knows,known,last,lately,later,latter,latterly,least,less,lest,let,let's,like,liked,likely,little,look,looking,looks,ltd,mainly,many,may,maybe,me,mean,meanwhile,merely,might,more,moreover,most,mostly,much,must,my,myself,name,namely,nd,near,nearly,necessary,need,needs,neither,never,nevertheless,new,next,nine,no,nobody,non,none,noone,nor,normally,not,nothing,novel,now,nowhere,obviously,of,off,often,oh,ok,okay,old,on,once,one,ones,only,onto,or,other,others,otherwise,ought,our,ours,ourselves,out,outside,over,overall,own,particular,particularly,per,perhaps,placed,please,plus,possible,presumably,probably,provides,quite,quote,quarterly,rather,really,reasonably,regarding,regardless,regards,relatively,respectively,right,said,same,saw,say,saying,says,second,secondly,see,seeing,seem,seemed,seeming,seems,seen,self,selves,sensible,sent,serious,seriously,seven,several,shall,she,should,shouldn't,since,six,so,some,somebody,somehow,someone,something,sometime,sometimes,somewhat,somewhere,soon,sorry,specified,specify,specifying,still,sub,such,sup,sure,t's,take,taken,tell,tends,than,thank,thanks,thanx,that,that's,thats,the,their,theirs,them,themselves,then,thence,there,there's,thereafter,thereby,therefore,therein,theres,thereupon,these,they,they'd,they'll,they're,they've,think,third,this,thorough,thoroughly,those,though,three,through,throughout,thru,thus,to,together,too,took,toward,towards,tried,tries,truly,try,trying,twice,two,under,unfortunately,unless,unlikely,until,unto,up,upon,us,use,used,useful,uses,using,usually,uucp,value,various,very,via,viz,vs,want,wants,was,wasn't,way,we,we'd,we'll,we're,we've,welcome,well,went,were,weren't,what,what's,whatever,when,whence,whenever,where,where's,whereafter,whereas,whereby,wherein,whereupon,wherever,whether,which,while,whither,who,who's,whoever,whole,whom,whose,why,will,willing,wish,with,within,without,won't,wonder,would,would,wouldn't,yes,yet,you,you'd,you'll,you're,you've,your,yours,yourself,yourselves,zero,official,sharply,criticized".split(
@@ -52,11 +52,11 @@ export class WordCloudComponent implements OnChanges {
                 return count;
             }, {});
 
-            var width = this.width > this.height ? this.height: this.width;
+        var width = this.width > this.height ? this.height : this.width;
 
         this.data = Object.keys(counts)
             .map(function(key) {
-                return { text: key, size: counts[key] * (width/70) };
+                return { text: key, size: counts[key] * (width / 70) };
             })
             .sort(function(a, b) {
                 return a.size === b.size ? 0 : a.size > b.size ? -1 : 1;
@@ -82,8 +82,9 @@ export class WordCloudComponent implements OnChanges {
 
     private buildSVG() {
         if (this.svg) {
-
-            D3.select('div.word-cloud').select('svg').remove();
+            D3.select('div.word-cloud')
+                .select('svg')
+                .remove();
         }
 
         this.svg = D3.select('div.word-cloud')
@@ -115,15 +116,15 @@ export class WordCloudComponent implements OnChanges {
                 ? 'archimedean'
                 : this.wordData.settings.spiral;
 
-var size = this.width >this.height ? this.height: this.width;
+        var size = this.width > this.height ? this.height : this.width;
 
         d3.layout
             .cloud()
-            .size([size,size])
+            .size([size, size])
             .words(this.data)
-            .padding(2)
+            .padding(3)
             .rotate(() => (~~(Math.random() * 6) - 3) * 30)
-            .font(fontFace)
+            .font("Impact")
             .fontWeight(fontWeight)
             .fontSize(d => d.size)
             .spiral(spiralType)
