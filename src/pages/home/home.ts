56fd9f4701192320ced558fb812303681d965221
julian perrott <julian_perrott@hotmail.com>
2018-06-04 20:54:56 +0100

    Always refresh links

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,26 +15,31 @@ import { HtmlToLinksService, Link } from '../../app/htmlToLinks.service';
 export class HomePage {
     @ViewChild(Content) content: Content;
     data = 'How the Word Cloud Generator Works The layout algorithm for positioning page without overlap is available on GitHub under an open source license as d3-cloud. Note that this is the only the layout algorithm and any code for converting text into page and rendering the final output requires additional development. As word placement can be quite slow for more than a few hundred page, the layout algorithm can be run asynchronously, with a configurable time step size. This makes it possible to animate page as they are placed without stuttering. It is recommended to always use a time step even without animations as it prevents the browser’s event loop from blocking while placing the page. The layout algorithm itself is incredibly simple. For each word, starting with the most “important”: Attempt to place the word at some starting point: usually near the middle, or somewhere on a central horizontal line. If the word intersects with any previously placed page, move it one step along an increasing spiral. Repeat until no intersections are found The hard part is making it perform efficiently! According to Jonathan Feinberg, Wordle uses a combination of hierarchical bounding boxes and quadtrees to achieve reasonable speeds. Glyphs in JavaScript There isn’t a way to retrieve precise glyph shapes via the DOM, except perhaps for SVG fonts. Instead, we draw each word to a hidden canvas element, and retrieve the pixel data. Retrieving the pixel data separately for each word is expensive, so we draw as many page as possible and then retrieve their pixels in a batch operation. clouds and Masks My initial implementation performed collision detection using cloud masks. Once a word is placed, it doesnt move, so we can copy it to the appropriate position in a larger cloud representing the whole placement area.The advantage of this is that collision detection only involves comparing a candidate cloud with the relevant area of this larger cloud, rather than comparing with each previous word separately.Somewhat surprisingly, a simple low-level hack made a tremendous difference: when constructing the cloud I compressed blocks of 32 1-bit pixels into 32-bit integers, thus reducing the number of checks (and memory) by 32 times.In fact, this turned out to beat my hierarchical bounding box with quadtree implementation on everything I tried it on (even very large areas and font sizes). I think this is primarily because the cloud version only needs to perform a single collision test per candidate area, whereas the bounding box version has to compare with every other previously placed word that overlaps slightly with the candidate area.Another possibility would be to merge a word’s tree with a single large tree once it is placed. I think this operation would be fairly expensive though compared with the analagous cloud mask operation, which is essentially ORing a whole block.';
-    links: Link[] = [];
-    refreshLinks = true;
+    links: Link[] = [
+        { href: 'https://www.bbc.co.uk/news', text: 'BBC News' } as Link,
+        { href: 'https://www.bbc.co.uk/sport', text: 'BBC Sport' } as Link,
+        { href: 'https://uk.reuters.com/', text: 'Reuters Uk' } as Link,
+        { href: 'https://edition.cnn.com/', text: 'CNN' } as Link
+    ];
+    refreshLinks = false;
 
     // url = 'http://www.capita.com/about-us/case-studies/how-we-helped-a-council-to-help-children-and-families/';
-    url = 'https://www.bbc.co.uk/news';
+    url = '';
+    lastUrl = '';
 
     constructor(
         private configurationService: ConfigurationService,
         private htmlToLinksService: HtmlToLinksService
     ) {
         configurationService.UrlChangeSource$.subscribe(v => {
-            this.refreshLinks = false;
+            this.refreshLinks = true;
             this.url = ConfigurationService.url;
             this.content.scrollToTop();
+            this.refresh();
         });
     }
 
     ionViewDidLoad() {
-        this.refresh();
-        // this.url = "";
         this.intro();
     }
 
@@ -76,6 +81,11 @@ export class HomePage {
             return;
         }
 
+        if (this.url === this.lastUrl) {
+            return;
+        }
+        this.lastUrl = this.url;
+
         this.configurationService.setBusy(true);
 
         const proxyurl = 'https://cors-anywhere.herokuapp.com/';
@@ -113,6 +123,7 @@ export class HomePage {
             }
             console.log(f.href + ',' + f.text);
         });
+
         this.links = links;
     }
 
